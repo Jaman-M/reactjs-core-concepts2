a28808ce28dc9f662bdabb4d249e6d5b93b464a9
Jaman-M <jaman.manik1@gmail.comm>
2022-03-19 12:13:43 +0600

    state,component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  const products=[
-    {name: 'laptop', price: 55000},
-    {name: 'phone', price: 30000},
-    {name: 'watch', price: 1000},
-    {name: 'tablet', price: 20000}
-  ]
+  // const products=[
+  //   {name: 'laptop', price: 55000},
+  //   {name: 'phone', price: 30000},
+  //   {name: 'watch', price: 1000},
+  //   {name: 'tablet', price: 20000}
+  // ]
   return (
     <div className="App">
-      {
+      {/* {
         products.map(product=> <Product name= {product.name} price= {product.price}></Product>)
-      }
+      } */}
 
       {/* <Product name="laptop" price="47000"></Product>
       <Product name="Phone" price="7500"></Product>
       <Product name="Watch" price="500"></Product> */}
       
+      <Counter></Counter>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(50);
+  // console.log(abc)
+
+  // const increaseCount = () => {
+  //   const newCount = count+1;
+  //   setCount(newCount);
+
+  const increaseCount = () => setCount(count+1);
+  const decreaseCount = () => setCount(count-1);
+
+  return(
+    <div>
+      <h1>Count: {count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
 }
 
 function Product(props){
